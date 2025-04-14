58e72baa305cc9052df75af7a73c321b96d2e5cd
cabernaciak <cabernaciak@sei.cmu.edu>
2025-04-13 21:17:05 -0400

    just wrote top comment

---
+++ tests/thread_test_ring_buffer.c
@@ -0,0 +1,10 @@
+/**
+ * @file thread_test_ring_buffer.c
+ * @brief Tests for the threaded ring_buffer library.
+ *
+ * This file contains a set of threaded/integration tests to verify the correctness of concurrent functionality.
+ *
+ * Author: Catherine Bernaciak PhD
+ * Date: April 2025
+ */
+
