20ac02d8a57dc3b52d7a1bc02fe4a8a9750ba583
cabernaciak <cabernaciak@sei.cmu.edu>
2025-04-18 15:14:28 -0400

    implemented pressure tests. they all pass

--- tests/thread_test_ring_buffer.c
+++ tests/thread_test_ring_buffer.c
@@ -6,16 +6,16 @@
  * correctness of concurrent functionality. We need to verify that concurrent
  * access from separate producer and consumer threads behaves properly: no
  * crashes, dropped values, or memory leaks.  These tests involve 2 or more 
- * threads accessing the same buffer concurrently. Concurrency test typs include:
- * - Basic Concurrency Check - constant but different rates of read & write
+ * threads accessing the same buffer concurrently. Concurrency tests are:
  * - Stress Tests:
- *      - Backpressure - high-frequency producer, slow consumer
- *      - Negative backpressure - slow producer, high-frequency consumer
- *      - Long-running wraparound 
- *      - Jittery producer and consumer 
- *      - Multiple producers and consumers
+ *    - Basic Concurrency Check - identical, constant rates of read & write
+ *    - Backpressure - high-frequency producer, slow consumer
+ *    - Negative backpressure - slow producer, high-frequency consumer
+ *    - Long-running wraparound 
+ *    - Jittery producer and consumer 
+ *    - Multiple producers and consumers
  * - Edge Test:
- *      - Mutex starvation / deadlock detection
+ *    - Mutex starvation / deadlock detection
  * Author: Catherine Bernaciak PhD
  * Date: April 2025
  */
@@ -27,10 +27,10 @@
 #include "ring_buffer.h"
 #include "test_helpers.h"
 
-//#define NUM_WRITES 10000
-#define NUM_WRITES 10 
-//#define BUFFER_CAPACITY 1000
-#define BUFFER_CAPACITY 4
+#define NUM_WRITES 10000
+//#define NUM_WRITES 10 
+#define BUFFER_CAPACITY 1000
+//#define BUFFER_CAPACITY 4
 
 typedef struct {
    ring_buffer *rb;
@@ -51,17 +51,20 @@ typedef struct {
  * returns void pointer
 */
 void *producer_thread(void *arg){
-  
+   //printf("producer thread ==========\n"); 
    // RHS = cast input void *arg to a thread_args* type
    // LHS = declare new variable *args that's a pointer to a 
    // thread_args struct
    thread_args *args = (thread_args *)arg;
+   int write_count = 0;
 
    // write continuously on this thread
-   for (int i = 0; i < NUM_WRITES; i++){
+   while(write_count < NUM_WRITES){
       float val = (float)(rand() % 1000);
       if (ring_buffer_write(args->rb, val)){
-         args->written_vals[i] = val;
+         //printf("value written in producer thread: %f\n", val);
+         args->written_vals[write_count] = val;
+         write_count++;
       } 
       usleep(args->write_rate); 
    }
@@ -80,16 +83,20 @@ void *producer_thread(void *arg){
 */
 void *consumer_thread(void *arg){
 
+   //printf("consumer thread ==========\n"); 
    // RHS = cast input void *arg to a thread_args * type
    // LHS = declare new variable *args that's a pointer to a 
    // thread_args struct
    thread_args *args = (thread_args *)arg;
    float read_val;
+   int read_count = 0;
 
    // perform reads on consumer thread
-   for (int i = 0; i < NUM_WRITES; i++){
+   while(read_count < NUM_WRITES){
       if (ring_buffer_read(args->rb, &read_val)){
-         args->read_vals[i] = read_val;
+         //printf("value read in consumer thread: %f\n", read_val);
+         args->read_vals[read_count] = read_val;
+         read_count++;
       }
       usleep(args->read_rate); 
    }
@@ -97,25 +104,35 @@ void *consumer_thread(void *arg){
 }
 
 /*
- * void thread_basic_concurrency()
- * Tests that ring buffer can handle a large number of concurrent
- * writes and reads. The rate of writes and reads is equal. 
- * Ring buffer is allocated and destroyed within this function.
- * There is wraparound. 
+ * void thread_pressure()
+ * This a stress test for the concurrent ring buffer. It allows
+ * for constant rw (basic), faster writes than reads (backpressure), 
+ * faster reads than writes (negative backpressure).  Rate is set
+ * by two inputs, which represent the integer number of microseconds 
+ * delay between reads or writes. Ring buffer is allocated and destroyed 
+ * within this function. There is wraparound. 
  *
- * input void
+ * inputs int write_rate, int read_rate
  * returns void
 */
-void thread_basic_concurrency(){
-   printf("[TEST] Basic Concurrecy Check ... \n");
+void thread_pressure(int write_rate, int read_rate){
+
+   if (write_rate == read_rate){
+      printf("[TEST] Basic Concurrency Check ... \n");
+   } else if (write_rate > read_rate){
+      printf("[TEST] Concurrent Negative Backpressure Check ... \n");
+   } else {
+      printf("[TEST] Concurrent Backpressure Check ... \n");
+   }
+
    // setup ring buffer and read/write comparison arrays 
    // inside of a thread_args struct
    thread_args *thargs = malloc(sizeof(thread_args)); 
    thargs->rb = malloc(sizeof(ring_buffer));
    thargs->written_vals = malloc(sizeof(float) * NUM_WRITES);
    thargs->read_vals = malloc(sizeof(float) * NUM_WRITES);
-   thargs->write_rate = 0;
-   thargs->read_rate = 0;
+   thargs->write_rate = write_rate; // # us between writes
+   thargs->read_rate = read_rate;   // # us between reads
 
    // some simple tests
    assert(thargs != NULL);
@@ -151,66 +168,13 @@ void thread_basic_concurrency(){
    free(thargs);
    printf("OK\n");
 }
-/*
- * void thread_backpressure()
- * Tests that ring buffer can handle a large number of concurrent
- * writes and reads where there are faster writes than reads.
- * Ring buffer is allocated and destroyed within this function. 
- * There is wraparound. 
- *
- * input void
- * returns void
-*/
-void thread_backpressure(){
-
-   printf("[TEST] Concurrent Backpressure Check ... \n");
-   // setup ring buffer and read/write comparison arrays 
-   // inside of a thread_args struct
-   thread_args *thargs = malloc(sizeof(thread_args)); 
-   thargs->rb = malloc(sizeof(ring_buffer));
-   thargs->written_vals = malloc(sizeof(float) * NUM_WRITES);
-   thargs->read_vals = malloc(sizeof(float) * NUM_WRITES);
-   thargs->write_rate = 0;
-   thargs->read_rate = 50;
-
-   // some simple tests
-   assert(thargs != NULL);
-   assert(thargs->rb != NULL);
-   assert(thargs->written_vals != NULL);
-   assert(thargs->read_vals != NULL);
-   assert(ring_buffer_init(thargs->rb, BUFFER_CAPACITY));
-
-   // pthread_t is an opaque type, holds thread ID's assigned by OS
-   pthread_t prod, cons;
-
-   // start first thread
-   pthread_create(&prod, NULL, producer_thread, thargs);
-   // return to main thread and begin executing this one
-   pthread_create(&cons, NULL, consumer_thread, thargs);
-
-   // tell main thread to wait until these threads finish before continuing
-   pthread_join(prod, NULL);
-   pthread_join(cons, NULL);
-
-   // test that all values written and read are the same - no loss
-   for (int i = 0; i < NUM_WRITES; i++){
-     printf("i ====================== %d\n", i);
-     printf("written_vals[i] = %f\n", thargs->written_vals[i]);
-     printf("read_vals[i] = %f\n", thargs->read_vals[i]);
-     assert(thargs->read_vals[i] == thargs->written_vals[i]);
-   }
-
-   // clean up the heap
-   free(thargs->written_vals);
-   free(thargs->read_vals);
-   SAFE_DESTROY(thargs->rb);
-   free(thargs);
-   printf("OK\n");
-}
 
 int main(){
-   thread_basic_concurrency();
-   thread_backpressure();
+   thread_pressure(100, 100);
+   thread_pressure(50, 100);
+   thread_pressure(100, 50);
+   //thread_basic_concurrency();
+   //thread_backpressure();
 
    return 0;
 }
