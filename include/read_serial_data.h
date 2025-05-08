cadf9a79c87614fc898b3e15dbae3aee9891e4cf
cabernaciak <cabernaciak@sei.cmu.edu>
2025-05-07 23:05:20 -0400

    updated to include the sine_data_stream function header

--- include/read_serial_data.h
+++ include/read_serial_data.h
@@ -1,1 +1,16 @@
-// update with data_stream_init function 
+
+// include guard
+#ifndef READ_SERIAL_DATA_H
+#define READ_SERIAL_DATA_H
+
+/*
+ * @brief Fills a ring buffer with sine wave data
+ *
+ * The function fills a ring buffer with data of sine wave moving 
+ * in the positive x direction. It's used as a placeholder for
+ * testing the eeg app before real brainwave data is available
+ * @param rb Pointer to a ring buffer instance
+ * @return void
+*/
+void sine_data_stream(ring_buffer *rb);
+
