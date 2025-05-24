3dd4f18c0f49f2f03e3bf0b1015f764cce43525d
cabernaciak <cabernaciak@sei.cmu.edu>
2025-05-23 23:16:04 -0400

    added support for interrupt control signal

--- include/eeg_config.h
+++ include/eeg_config.h
@@ -1,3 +1,15 @@
-#pragma once
+#ifndef EEG_CONFIG_H
+#define EEG_CONFIG_H
 
+#include <signal.h>
+
+/*
+ * This is for stopping the app. It's defined in int main of 
+ * eeg_app.c and used in void serial_reader of read_serial_data.c
+ */
+extern volatile sig_atomic_t keep_running; // for stopping the app
+
+#pragma once
 #define NUM_POINTS 512
+
+#endif
