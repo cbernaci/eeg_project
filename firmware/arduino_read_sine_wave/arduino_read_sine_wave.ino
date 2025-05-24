093f89742fbcb6fb5ab44ed5d07b5578003b6436
cabernaciak <cabernaciak@sei.cmu.edu>
2025-05-23 23:02:10 -0400

    for generating the sine wave sketch

--- firmware/arduino_read_sine_wave/arduino_read_sine_wave.ino
+++ firmware/arduino_read_sine_wave/arduino_read_sine_wave.ino
@@ -1,23 +1,22 @@
-// a sketch to generate a PWM square wave voltage (0V - 5V) from pin 9
-// then read voltage from analog pin A0 from Macbook
-// this is just an example to test serial data input to the Mac
-// I will use this stream to test a ring buffer data structure
-// A0 reads input voltage (square wave)
+// A sketch that outputs a sine wave voltage (0V - 5V) from pin 9
+// and sends to analog input pin A0. The signal is then serialized
+// by serial monitor and read via USB on MacBook. 
+// This tests serial data input to ring buffer & Metal visualization
 // Author: Catherine Bernaciak, PhD
-// Date: Mar 2025
+// Date: May 2025
 
 const int outputPin = 9;         // PWM output pin, 490 Hz default
 const int inputPin = A0; 
 const int sampleCount = 256;
 int sineWave[sampleCount];
+
 // this function executed only once when arduino starts or resets
 // must be here, even if empty
 void setup() {
 
    Serial.begin(115200);           // set baud rate to 115200 
    pinMode(outputPin, OUTPUT);     // configure pin 9 as output
-   // initialize sine wave lookup table
-   for (int i = 0; i < sampleCount; i++){
+   for (int i = 0; i < sampleCount; i++){  // initialize array with sine wave 
       sineWave[i] = 127 + 127*sin(2*PI*i/sampleCount);
    } 
 }
@@ -30,7 +29,7 @@ void loop() {
       delayMicroseconds(100);                            // let the signal propagate
       int sensorValue = analogRead(inputPin);            // read input signal on A0
       float voltage = sensorValue*(5.0/1023.0);          // convert to 0-5V
-      Serial.write((byte*)&voltage1, sizeof(voltage1));  // pass value to serial monitor to usb
+      Serial.write((byte*)&voltage, sizeof(voltage));  // pass value to serial monitor to usb
       //Serial.println(voltage1); // sends as string terminated with \r\n
       //delayMicroseconds(5);
       //Serial.flush();
